655578d01c2c6f5cd09ba2740648107b3d168124
Grzegorz Czekaj <gcz@sorz.local>
2019-07-19 14:23:13 +0200

    Make porfolio plugin

---
+++ plugins/grzegorzczekaj/portfolio/updates/builder_table_create_grzegorzczekaj_portfolio_works.php
@@ -0,0 +1,28 @@
+<?php namespace GrzegorzCzekaj\Portfolio\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateGrzegorzczekajPortfolioWorks extends Migration
+{
+    public function up()
+    {
+        Schema::create('grzegorzczekaj_portfolio_works', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->text('desc')->nullable();
+            $table->string('url')->nullable();
+            $table->boolean('show')->default(1);
+            $table->string('image');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('grzegorzczekaj_portfolio_works');
+    }
+}
